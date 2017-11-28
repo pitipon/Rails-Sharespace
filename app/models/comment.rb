2ad69a5c860c29098ad7f40f781c297165e2f80c
dominic currano <dominiccurrano@gmail.com>
2017-11-28 11:07:49 +0800

    added workspace and pic belong to and has many

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :workspace
+  belongs_to :user
+
+  validates :message, presense: :true
 end
--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,4 +1,8 @@
 class Workspace < ApplicationRecord
-  has_many :reviews
   belongs_to :owner
+  has_many :comments
+  has_many :orders
+  has_many :picture
+
+  validates :user_id, :owner_id
 end
