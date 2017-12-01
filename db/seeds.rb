0b0974c63fb06fb6452c7a3c2429095f0f67e1f0
ms <mrksauceda@gmail.com>
2017-12-01 14:32:40 +0800

    updated the owner html for show/index

--- db/seeds.rb
+++ db/seeds.rb
@@ -187,34 +187,6 @@ real_address = [
 ]
 
 
-SPACETYPE = ["Coworking","Meeting Room","Private Office","Remote Office","Cubicle","Board Room","Training Room","Presentation Room","Deposition Room","Personal Workspace","Desk Space","Shared Office Space","Document Review"]
-  # MINIMAL_SPACETYPE = ["Desk", "Meeting Space", "Office", "Training Room", "Event Space"]
-ENVIRONMENT = ["Coworking","Business Center", "Private Business", "Hotel", "University", "Apartment", "House", "Garden"]
-THEME = ["Business", "IT", "Design", "Financial", "Artist", "Relaxed"]
-AVAILABLE_TIME = ["7 Days, 24 Hours", "5 Days, 24 Hours", "7 Days, Business Hours", "5 Days, Business Hours"]
-CATERING = ["Coffee/Tea", "Partial Kitchen", "Full Kitchen"]
-SECURITY = ["Door Lock", "Lobby Security", "Security Guard"]
-MEDIA = ["Film Allowance", "Recording camera", "Social Media equipment", "YouTube Casting equipment"]
-SERVICES = ["Hosted Reception", "Mailbox Services", "Room Set-up", "IT Service", "Maid", "Body Guard"]
-FREEROOMS = ["Meeting Room", "Relax Space", "Exercise Room", "Take a nap Room", "Maker Room", "Smoking Room", "Private Telephone Room", "Shower Room", "Long Term / Short Term storage", "Exclusive Lounge"]
-PARKING = ["Bicycles", "Bike", "Cars", "Bus", "Truck", "VIP cars"]
-PET_FRIENDLY = ["none", "Dog", "Cat", "Any"]
-UTILITIES = ["Phone", "IP Phone", "Notebook", "Printer", "Scanner", "Copy Machine", "Wireless Hotspot", "Projector", "Pointer", "Maker ", "Paper", "Sofa", "Chair", "TV/Monitor", "Video Conference", "White Board","Air-conditioner", "Heater"]
-NEAR = ["Airport", "Underground", "Downtown", "Beach", "Mountain"]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 100.times do
   Workspace.create!(
